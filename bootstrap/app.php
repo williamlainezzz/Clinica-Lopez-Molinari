d4cb8a6de8c3b0d349b88c5700b101d828fa2920
RogerAle98 <rmurillog@unah.hn>
2025-10-06 21:56:34 -0600

    CAMBIO1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,3 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+
+    //ADSFASASFA}
+    //ASFASFASF
+
+    
