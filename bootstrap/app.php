074b83bc2c24c1e842f633b4ff894ba162d314b6
RogerAle98 <rmurillog@unah.hn>
2025-10-25 21:48:29 -0600

    Corrigiendo part3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,47 +1,22 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+$app = new Illuminate\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-    // GLOBALS (si necesitas alguno global, p.ej. TrimStrings, ConvertEmptyStringsToNull, etc.)
-    // $middleware->append(\App\Http\Middleware\TrimStrings::class);
-    // $middleware->append(\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class);
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    App\Http\Kernel::class
+);
 
-    // GRUPOS (equivalentes a $middlewareGroups de Kernel)
-    $middleware->web([
-        \App\Http\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \Illuminate\Session\Middleware\AuthenticateSession::class,
-        \App\Http\Middleware\VerifyCsrfToken::class,
-        \Illuminate\Routing\Middleware\SubstituteBindings::class,
-    ]);
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
 
-    $middleware->api([
-        \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
-        \Illuminate\Routing\Middleware\SubstituteBindings::class,
-    ]);
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
 
-    // ALIASES (equivalentes a $middlewareAliases / $routeMiddleware de Kernel)
-    $middleware->alias([
-        'auth'     => \App\Http\Middleware\Authenticate::class,
-        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-
-        // 👇 el que nos falta
-        'permiso'  => \App\Http\Middleware\CheckObjetoPermission::class,
-    ]);
-})
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+return $app;
