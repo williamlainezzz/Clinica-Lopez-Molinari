9accdea4cf7b61536a0603d3cf9396bb49267df7
RogerAle98 <rmurillog@unah.hn>
2025-11-02 20:34:49 -0600

    Agenda: rutas por rol + menú AdminLTE + limpieza

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,22 +1,20 @@
 <?php
 
-$app = new Illuminate\Foundation\Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-);
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
-$app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
-);
-
-return $app;
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        
+        commands: __DIR__.'/../routes/console.php',
+        // Si usas broadcasting:
+        // channels: __DIR__.'/../routes/channels.php',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
